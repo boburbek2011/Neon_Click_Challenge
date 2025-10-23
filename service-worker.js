f995dd3d7704e4d4d0e00ff6ddd15660144b1fb1
boburbek2011 <xasanboykenjayev08@gmail.com>
2025-10-23 18:49:21 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'neon-game-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './easylevel.html',
+  './hardlevel.html',
+  './extremelevel.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
